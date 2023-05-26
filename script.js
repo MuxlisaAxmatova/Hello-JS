19a862f50980ee589ced46e71daf0fc0cdc01edc
Muxlisa <axmatovamuxlisa800@gmail.com>
2023-05-26 14:56:07 +0500

    changed

---
+++ script.js
@@ -0,0 +1,21 @@
+var fruit = ["banan", "olma", "apelsin", "tarvuz"];
+//ikkinchi.
+fruit.shift()
+console.log(fruit)
+//uchinchi.
+console.log(fruit.indexOf("apelsin"))
+//beshinchi.
+fruit.push(4)
+console.log(fruit)
+
+
+
+
+
+var sabzavotlar = ["sabzi", "pomidor", "qalampir", "salat"];
+//birinchi.
+sabzavotlar.pop()
+console.log(sabzavotlar)
+//oltinchi.
+sabzavotlar.push(3)
+console.log(sabzavotlar)
